8b699903a310e94c6b9fd1065f2f9f9c27074edb
Jon Robison <narfman0@gmail.com>
2015-03-26 15:14:49 -0400

    -Add conky rc file -Change naming schema to be same as originals

--- apply.sh
+++ apply.sh
@@ -2,7 +2,9 @@
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 pushd ~
 mv .bash_profile .bash_profile.old
-ln $(SCRIPTPATH)/bash_profile .bash_profile
+ln $(SCRIPTPATH)/.bash_profile
 mv .vimrc .vimrc.old
-ln $(SCRIPTPATH)/vimrc .vimrc
+ln $(SCRIPTPATH)/.vimrc
+mv .conkyrc .conkyrc.old
+ln $(SCRIPTPATH)/.conkyrc
 popd
