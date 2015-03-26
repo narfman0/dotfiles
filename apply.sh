0adb55954df55b2c7e6d4a4e4c91026fd0d55148
Jon Robison <narfman0@gmail.com>
2015-03-26 15:24:43 -0400

    -Add status printout

--- apply.sh
+++ apply.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+export SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+echo 'Linking scripts to ' $SCRIPTPATH
 pushd ~
 mv .bash_profile .bash_profile.old
-ln $(SCRIPTPATH)/.bash_profile
+ln $SCRIPTPATH/.bash_profile
 mv .vimrc .vimrc.old
-ln $(SCRIPTPATH)/.vimrc
+ln $SCRIPTPATH/.vimrc
 mv .conkyrc .conkyrc.old
-ln $(SCRIPTPATH)/.conkyrc
+ln $SCRIPTPATH/.conkyrc
 popd
