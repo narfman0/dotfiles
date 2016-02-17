5a98a1a6f86a664f55017fce622c38b2bae7e76b
Jon Robison <narfman0@gmail.com>
2016-02-16 23:29:25 -0500

    -Refactor bash file to be common, so any unix can source it

--- apply.sh
+++ apply.sh
@@ -2,10 +2,13 @@
 export SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 echo 'Linking scripts to ' $SCRIPTPATH
 pushd ~
-mv .bash_profile .bash_profile.old
-ln $SCRIPTPATH/.bash_profile
+
+echo . $SCRIPTPATH/.bash_common >> .bashrc
+
 mv .vimrc .vimrc.old
 ln $SCRIPTPATH/.vimrc
+
 mv .conkyrc .conkyrc.old
 ln $SCRIPTPATH/.conkyrc
+
 popd
