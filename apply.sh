1875c087ee3f2a9d3c1b1dec9a7b722417b8a938
Jon Robison <narfman0@gmail.com>
2016-12-14 23:28:25 -0500

    -Add znc password input to apply -Use much newer advanced window for irssi

--- apply.sh
+++ apply.sh
@@ -19,6 +19,12 @@ mkdir -p .tmux
 ln -fs $SCRIPTPATH/.tmux.conf
 ln -fs $SCRIPTPATH/.tmuxdev -T .local/bin/tmuxdev
 
-ln -fs $SCRIPTPATH/.irssi
+echo -n "What is your ZNC password (for irssi)? "
+read -s znc_pass
+mkdir -p ~/.irssi
+ln -fs $SCRIPTPATH/.irssi/scripts ~/.irssi/
+ln -fs $SCRIPTPATH/.irssi/weed.theme ~/.irssi/
+cp $SCRIPTPATH/.irssi/config ~/.irssi/
+sed -i -e "s/ZNC_PASS/$znc_pass/g" ~/.irssi/config
 
 popd
