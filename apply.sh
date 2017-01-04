df8764a27b1f0229e495b74287e4d309a9a8d90f
Jon Robison <narfman0@gmail.com>
2017-01-03 21:31:28 -0500

    -Save pass in environment variable

--- apply.sh
+++ apply.sh
@@ -17,8 +17,11 @@ mkdir -p .tmux
 mv .tmux.conf .tmux.conf.old
 ln -fs $SCRIPTPATH/.tmux.conf
 
-echo -n "What is your ZNC password (for irssi)? "
-read -s znc_pass
+if [ -z "$ZNC_PASS" ]; then
+  echo -n "What is your ZNC password (for irssi)? "
+  read -s ZNC_PASS
+  echo "export ZNC_PASS=$ZNC_PASS" >> ~/.bashrc
+fi
 rm -rf ~/.irssi.old
 mv ~/.irssi ~/.irssi.old
 mkdir -p ~/.irssi
@@ -26,7 +29,7 @@ ln -fs $SCRIPTPATH/.irssi/scripts ~/.irssi/
 ln -fs $SCRIPTPATH/.irssi/weed.theme ~/.irssi/
 ln -fs $SCRIPTPATH/.irssi/default.theme ~/.irssi/
 cp $SCRIPTPATH/.irssi/config ~/.irssi/
-sed -i -e "s/ZNC_PASS/$znc_pass/g" ~/.irssi/config
+sed -i -e "s/ZNC_PASS/$ZNC_PASS/g" ~/.irssi/config
 
 mkdir -p ~/.newsbeuter
 ln -fs $SCRIPTPATH/.newsbeuter/urls ~/.newsbeuter/
