0288e992076e842119fe475dd860a58c7091493d
Jon Robison <narfman0@gmail.com>
2019-02-21 13:56:13 -0500

    -Update some steps for pipenv

--- install.sh
+++ install.sh
@@ -49,9 +49,9 @@ elif [ "$OS" == "Darwin" ]; then
     # required before sshfs
     brew cask install osxfuse
     brew install vim-nox --with-override-system-vi
-    brew install cmake jq mutt pipenv pyenv sshfs terraform vault
+    brew install cmake jq mutt pyenv sshfs terraform vault
     brew cask install intellij-ce visual-studio-code firefox google-chrome
-    echo "Please setup pyenv to install py3 :)"
+    echo "Please setup pyenv to install py3 and pipenv :)"
 else
     echo "Operating system: $OS not recognized :("
 fi
