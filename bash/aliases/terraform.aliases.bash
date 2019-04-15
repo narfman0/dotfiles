85c7b4d27bed9439bbccc706fa23f5c9ff36a739
Jon Robison <narfman0@gmail.com>
2019-04-15 11:42:53 +0200

    -Lean on bashit a tad more. Still unwilling to clone the whole thing though :)

---
+++ bash/aliases/terraform.aliases.bash
@@ -0,0 +1,16 @@
+cite 'about-alias'
+about-alias 'terraform aliases'
+
+# Aliases
+alias tfcvfs='if [ -f "secrets.tfvars" ]; then echo "--var-file=secrets.tfvars"; fi'
+alias tfinit='terraform init $(tfcvfs)'
+alias tfci='if [ ! -d ".terraform" ]; then tfinit; fi'
+alias tfa='tfci; terraform apply $(tfcvfs)'
+alias tfaa='tfci; terraform apply -auto-approve $(tfcvfs)'
+alias tfd='tfci; terraform destroy $(tfcvfs)'
+alias tffu='tfci; terraform force-unlock $(tfcvfs)'
+alias tfi='tfci; terraform import $(tfcvfs)'
+alias tfo='tfci; terraform output $(tfcvfs)'
+alias tfp='tfci; terraform plan $(tfcvfs)'
+alias tfs='tfci; terraform state $(tfcvfs)'
+alias tffmt='terraform fmt'
