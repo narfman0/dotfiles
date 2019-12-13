2b810b45254930a849fa3abbe2dd1e94c8c685d5
Jon Robison <narfman0@gmail.com>
2019-12-12 22:12:10 -0500

    -Add ruby (with rails) to auto install

--- apply.sh
+++ apply.sh
@@ -52,6 +52,8 @@ popd
 
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable --rails
 
 popd
 
