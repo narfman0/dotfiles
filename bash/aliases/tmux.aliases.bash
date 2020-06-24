cd01894ee7db93204905342403e242491afd0ae6
Jon Robison <narfman0@gmail.com>
2020-06-24 16:13:29 -0400

    -Adding a few options which will solve 99.9999 percent of what i attach to

--- bash/aliases/tmux.aliases.bash
+++ bash/aliases/tmux.aliases.bash
@@ -2,11 +2,13 @@ alias tsa='tmux attach-session'
 alias tsk='tmux kill-session'
 alias tsl='tmux list-sessions'
 alias tsn='tmux new-session'
-alias tsnn='tmux new-session -s newsbeuter newsbeuter'
 alias tsan='tmux attach-session -t newsbeuter'
 alias tsai='tmux attach-session -t irssi'
 alias tsa0='tmux attach-session -t 0'
+alias tsa1='tmux attach-session -t 1'
+alias tsa2='tmux attach-session -t 2'
 alias tsni='tmux new-session -s irssi irssi'
+alias tsnn='tmux new-session -s newsbeuter newsbeuter'
 tsnd() {
     tmux new-session -d bash
     tmux split-window -v
