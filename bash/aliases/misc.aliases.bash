2c7c17478ff2c4d7c694a176a75547a9b6011bce
Jon Robison <narfman0@gmail.com>
2021-02-01 12:54:32 -0500

    Make ag -q alias agq

--- bash/aliases/misc.aliases.bash
+++ bash/aliases/misc.aliases.bash
@@ -9,6 +9,7 @@ alias ampc='cp $1 `grep UUID -A1 -a $1 | grep -io "[-A-Z0-9]\{36\}"`'
 alias beep='echo -e "\a"'
 alias rustup-init='curl --proto '"'"'=https'"'"' --tlsv1.2 -sSf https://sh.rustup.rs | sh'
 alias cte='crontab -e'
+alias agq='ag -q'
 
 alias genpass="openssl rand -base64 20"
 alias c='clear'
