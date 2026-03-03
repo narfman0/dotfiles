8535d6876687c3727277c70ad11305f1abc5bf59
Jon Robison <narfman0@gmail.com>
2026-03-03 08:58:41 -0500

    Add pythoninit function for vim plugin installation
    Add pythoninit function to install vim plugins and compile YCM.

--- bash/misc/misc.bash
+++ bash/misc/misc.bash
@@ -4,6 +4,17 @@ newsbeuterpurge() {
 osxaslclean() {
     sudo mv /private/var/log/asl/*.asl ~/.Trash
 }
+pythoninit() {
+    echo "Installing vim plugins"
+    vim -E -s -u "~/.vimrc" +PluginInstall +qall
+    pushd .vim/bundle/YouCompleteMe
+    if [ ! -e "third_party/ycmd/ycm_core.so" ]; then
+        echo "YCM not compiled, compiling"
+        ./install.py
+    else
+        echo "YCM already compiling, skipping"
+    fi
+}
 function cd(){
     builtin cd "${*}"
     if [ -e ".env" ]; then
