2b61583d94b1ca1e4990b83098a3faa1c7589a69
Jon Robison <narfman0@gmail.com>
2026-08-13 20:44:16 -0400

    incremental commit 2026-08-13 20:44:16

--- bash/aliases/misc.aliases.bash
+++ bash/aliases/misc.aliases.bash
@@ -4,29 +4,32 @@ alias ll='ls -FGlAhp'
 alias lll='ll | count'
 alias rf='find . 2> /dev/null | grep'
 alias striphtml="sed 's|<[^>]*>||g'"
-alias pt='python -m unittest discover'
-alias ampc='cp $1 `grep UUID -A1 -a $1 | grep -io "[-A-Z0-9]\{36\}"`'
 alias beep='echo -e "\a"'
 alias rustup-init='curl --proto '"'"'=https'"'"' --tlsv1.2 -sSf https://sh.rustup.rs | sh'
 alias cte='crontab -e'
 alias agq='ag -q'
 alias vim='nvim'
-alias vimb='vim ~/.bashrc'
-alias vimbs='vim ~/.bashrc && source ~/.bashrc'
+alias vimb='vim ~/.bashrc && source ~/.bashrc'
+alias cdw='cd ~/workspace'
 wex(){
-    while :
-    do
-        $@
-        sleep 1
-    done
+  while :
+  do
+    $@
+    sleep 1
+  done
 }
 
 alias genpass="openssl rand -base64 20"
 alias c='clear'
 alias ipe='curl ipinfo.io/ip'
-#alias agr='ag "$1" --nogroup | awk '{print substr($1,1,index($1,":")-1);}' | xargs -I {} sed -i '.back' -e "s/$1/$2/g" {}'
 alias dice='dice.sh'
 alias roll='dice'
+dfcpo() {
+  pushd $DOTFILES_ROOT
+  git commit -am "incremental commit $(date +'%Y-%m-%d %H:%M:%S')"
+  git push origin
+  popd
+}
 
 if [ -x "$(command -v trash)" ]; then
     alias rm='trash'
